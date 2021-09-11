ec2e04f8ccfde6a8a6ca739a4e74ee418847a61d
hamedghd <h.gharibdoost@gmail.com>
2021-09-11 17:04:13 +0200

    Add some code to make the app to navigate from login to movies

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Router } from '@angular/router';
 // You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
 
@@ -23,7 +23,8 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    public router: Router
   ) { }
 
   ngOnInit(): void {
@@ -41,6 +42,7 @@ export class UserLoginFormComponent implements OnInit {
         duration: 2000,
         panelClass: ['success-snackbar'],
       });
+      this.router.navigate(['movies']);
     }, (response) => {
       this.snackBar.open(response, 'Error', {
         duration: 2000,
