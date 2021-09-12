810e8ec870e1cc17db3e924cb4e9b51f799c97ce
hamedghd <h.gharibdoost@gmail.com>
2021-09-12 17:51:34 +0200

    Create movi-card component

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -8,7 +11,11 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 })
 export class MovieCardComponent {
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+  ) { }
 
   ngOnInit(): void {
     this.getMovies();
@@ -20,5 +27,12 @@ export class MovieCardComponent {
       console.log(this.movies);
       return this.movies;
     });
+  }
+
+  openSynopsisDialog(title: string, imageUrl: any, description: string): void {
+    this.dialog.open(MovieSynopsisComponent, {
+      data: { title, imageUrl, description, },
+      width: '500px'
+    })
   }
 }
